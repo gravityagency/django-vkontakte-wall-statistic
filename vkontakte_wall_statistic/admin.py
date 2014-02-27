c26b4c8731ebc94befb45440ca5a052962fd5a89
ramusus <ramusus@gmail.com>
2014-02-28 00:06:19 +0530

    added admin integration, made field null=False

--- vkontakte_wall_statistic/admin.py
+++ vkontakte_wall_statistic/admin.py
@@ -1,18 +1,22 @@
-# # -*- coding: utf-8 -*-
-# from django.contrib import admin
-# from vkontakte_api.admin import VkontakteModelAdmin
-# from vkontakte_groups.admin import Group, GroupAdmin as GroupAdminOriginal
-# from models import GroupStat
-#
-# class GroupStatInline(admin.TabularInline):
-#     model = GroupStat
-#     fields = ('date','visitors','views','likes','comments','shares','new_members','ex_members','members','ads_visitors','ads_members','males','females')
-#     readonly_fields = fields
-#     extra = 0
-#     can_delete = False
-#
-# class GroupAdmin(GroupAdminOriginal):
-#     inlines = GroupAdminOriginal.inlines + [GroupStatInline]
-#
-# admin.site.unregister(Group)
-# admin.site.register(Group, GroupAdmin)
+# -*- coding: utf-8 -*-
+from django.contrib import admin
+from vkontakte_wall.admin import Post, PostAdmin as PostAdminOriginal
+from models import PostStatistic
+
+class PostStatisticInline(admin.TabularInline):
+    model = PostStatistic
+    fields = ('date', 'reach', 'reach_subscribers', 'link_clicks',
+        'reach_males', 'reach_females',
+        'reach_age_18', 'reach_age_18_21', 'reach_age_21_24', 'reach_age_24_27', 'reach_age_27_30', 'reach_age_30_35', 'reach_age_35_45', 'reach_age_45',
+        'reach_males_age_18', 'reach_males_age_18_21', 'reach_males_age_21_24', 'reach_males_age_24_27', 'reach_males_age_27_30', 'reach_males_age_30_35', 'reach_males_age_35_45', 'reach_males_age_45',
+        'reach_females_age_18', 'reach_females_age_18_21', 'reach_females_age_21_24', 'reach_females_age_24_27', 'reach_females_age_27_30', 'reach_females_age_30_35', 'reach_females_age_35_45', 'reach_females_age_45',
+    )
+    readonly_fields = fields
+    extra = 0
+    can_delete = False
+
+class PostAdmin(PostAdminOriginal):
+    inlines = PostAdminOriginal.inlines + [PostStatisticInline]
+
+admin.site.unregister(Post)
+admin.site.register(Post, PostAdmin)
